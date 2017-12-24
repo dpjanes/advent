284abba36f4d6aa9136b767ea1634d8e9e09af48
David Janes <david@consensas.com>
2017-12-24 11:36:25 -0330

    better naming

--- day_23_2_opt.c
+++ day_23_2_opt.c
@@ -2,35 +2,28 @@
 #include <stdlib.h>
 int main()
 {
-    int e = 0;
-    int f = 0;
-    int g = 0;
-    int h = 0;
-
+    int count = 0;
     int start = 65 * 100 + 100000;
     int end = start + 17000;
 
     for (int outer = start; outer <= end; outer += 17) {
         printf("%d/%d\n", outer, end);
 
-        f = 1;
+        int is_prime = 1;
 
-        for (int middle = 2; middle <= outer; middle++) {
-            for (int inner = 2; inner <= outer; inner++) {
-                g = middle * inner - outer;
-                printf("middle=%d inner=%d outer=%d\n", middle, inner, outer);
-                if (!g) {
-                    exit(0);
-                    f = 0;
+        for (int middle = 2; middle <= outer && is_prime; middle++) {
+            for (int inner = 2; inner <= outer && is_prime; inner++) {
+                if ((middle * inner - outer) == 0) {
+                    is_prime = 0;
                 }
             }
-        };
-
-        if (!f) {
-            h ++;
         }
-    };
-step32:
-   printf("h: %d\n", h);
-   return 0;
+
+        if (!is_prime) {
+            count ++;
+        }
+    }
+
+    printf("count: %d\n", count);
+    return 0;
 }
