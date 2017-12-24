aa7f499fef3c96260ac3c826228ad40e4324538e
David Janes <david@consensas.com>
2017-12-24 11:43:22 -0330

    final optimization

--- day_23_2_opt.c
+++ day_23_2_opt.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+
 int main()
 {
     int count = 0;
@@ -7,10 +9,18 @@ int main()
     int end = start + 17000;
 
     for (int outer = start; outer <= end; outer += 17) {
-        printf("%d/%d\n", outer, end);
+        // printf("%d/%d\n", outer, end);
 
-        int is_prime = 1;
+        int is_prime = outer % 2;
 
+        int max_divisor = sqrt(outer) + 1;
+        for (int divisor = 3; (divisor < max_divisor) && is_prime; divisor += 2) {
+            if ((outer % divisor) == 0) {
+                is_prime = 0;
+            }
+        }
+
+        /*
         for (int middle = 2; middle <= outer && is_prime; middle++) {
             for (int inner = 2; inner <= outer && is_prime; inner++) {
                 if ((middle * inner - outer) == 0) {
@@ -18,6 +28,7 @@ int main()
                 }
             }
         }
+        */
 
         if (!is_prime) {
             count ++;
