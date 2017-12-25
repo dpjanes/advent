081d8c8e81f5ab84d8a5e55f9dc33625f31ee340
David Janes <david@consensas.com>
2017-12-25 04:30:40 -0330

    Finished Day 25/1

--- day_25_1.js
+++ day_25_1.js
@@ -36,12 +36,12 @@ const parse = raw => {
                         {
                             state: null,
                             move: null,
-                            write: null,
+                            write: 0,
                         },
                         {
                             state: null,
                             move: null,
-                            write: null,
+                            write: 1,
                         },
                     ]
                 }
@@ -82,7 +82,68 @@ const parse = raw => {
     return program;
 }
 
-console.log(parse(`
+const Turing = program => {
+    const self = Object.assign({});
+
+    let min = 0;
+    let max = 0;
+    let pc = 0;
+    let state = program.state;
+
+    const tape = {};
+    const get = position => tape[position] || 0;
+    const set = (position, value) => {
+        min = Math.min(position, min)
+        max = Math.max(position, max)
+
+        if (value) {
+            tape[position] = 1;
+        } else {
+            delete tape[position];
+        }
+    }
+
+    self.pretty = () => {
+        const parts = []
+
+        for (let pi = min; pi <= max; pi++) {
+            if (pi === pc) {
+                parts.push(`(${get(pi)})`)
+            } else {
+                parts.push(`${get(pi)}`)
+            }
+        }
+
+        return parts.join(" ");
+    }
+
+    self.step = () => {
+        const rule = program.states[state].rules[get(pc)];
+
+        state = rule.state;
+        set(pc, rule.write)
+        pc += rule.move;
+    }
+
+    self.run = () => {
+        console.log(`* 0:`, self.pretty())
+        for (let i = 0; i < program.steps; i++) {
+            self.step();
+            if (!(i % 10000)) {
+                console.log(i)
+                // console.log(`* ${i+1}:`, self.pretty())
+            }
+        }
+
+        return Object.keys(tape).length;
+    }
+
+    return self;
+}
+
+const run = raw => Turing(parse(raw)).run()
+
+console.log(run(`
 Begin in state A.
 Perform a diagnostic checksum after 6 steps.
 
@@ -105,4 +166,68 @@ In state B:
     - Write the value 1.
     - Move one slot to the right.
     - Continue with state A.
+`))
+console.log(run(`
+Begin in state A.
+Perform a diagnostic checksum after 12586542 steps.
+
+In state A:
+  If the current value is 0:
+    - Write the value 1.
+    - Move one slot to the right.
+    - Continue with state B.
+  If the current value is 1:
+    - Write the value 0.
+    - Move one slot to the left.
+    - Continue with state B.
+
+In state B:
+  If the current value is 0:
+    - Write the value 0.
+    - Move one slot to the right.
+    - Continue with state C.
+  If the current value is 1:
+    - Write the value 1.
+    - Move one slot to the left.
+    - Continue with state B.
+
+In state C:
+  If the current value is 0:
+    - Write the value 1.
+    - Move one slot to the right.
+    - Continue with state D.
+  If the current value is 1:
+    - Write the value 0.
+    - Move one slot to the left.
+    - Continue with state A.
+
+In state D:
+  If the current value is 0:
+    - Write the value 1.
+    - Move one slot to the left.
+    - Continue with state E.
+  If the current value is 1:
+    - Write the value 1.
+    - Move one slot to the left.
+    - Continue with state F.
+
+In state E:
+  If the current value is 0:
+    - Write the value 1.
+    - Move one slot to the left.
+    - Continue with state A.
+  If the current value is 1:
+    - Write the value 0.
+    - Move one slot to the left.
+    - Continue with state D.
+
+In state F:
+  If the current value is 0:
+    - Write the value 1.
+    - Move one slot to the right.
+    - Continue with state A.
+  If the current value is 1:
+    - Write the value 1.
+    - Move one slot to the left.
+    - Continue with state E.
 `))
