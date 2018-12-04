2bdacd1742d4a50dd8f0e5aa53aae376dc3642a9
David Janes <david@consensas.com>
2018-12-04 07:33:05 -0500

    stubs!

---
+++ 2018/day_15_1/main.go
@@ -0,0 +1,33 @@
+// http://adventofcode.com/2018/day/15
+
+package main
+
+import "fmt"
+import "io/ioutil"
+import "strings"
+import "strconv"
+
+func check(e error) {
+    if e != nil {
+        panic(e)
+    }
+}
+
+func main() {
+    /*
+    dat, err := ioutil.ReadFile("data.dat")
+    check(err)
+
+    s_changes := strings.Split(string(dat), "\n")
+
+    frequency := 0
+    for _, s_change := range s_changes {
+        i_change, err := strconv.Atoi(s_change)
+        if err == nil {
+            frequency += i_change
+        }
+    }
+
+    fmt.Printf("%v\n", frequency)
+    */
+}
