091e59f7943d4f697cf1f02217acf3b5f443963c
David Janes <david@consensas.com>
2018-12-04 08:41:01 -0500

    solved

--- 2018/day_02_1/main.go
+++ 2018/day_02_1/main.go
@@ -5,7 +5,7 @@ package main
 import "fmt"
 import "io/ioutil"
 import "strings"
-import "strconv"
+// import "strconv"
 
 func check(e error) {
     if e != nil {
@@ -13,21 +13,38 @@ func check(e error) {
     }
 }
 
+func counts(s string) (map[int]int) {
+    seen := map[string]int {}
+    for _, c := range strings.Split(s, ""){
+        seen[c] += 1
+    }
+
+    c := map[int]int {}
+    for _, value := range seen {
+        c[value] += 1
+    }
+
+    return c
+}
+
 func main() {
-    /*
     dat, err := ioutil.ReadFile("data.dat")
     check(err)
 
-    s_changes := strings.Split(string(dat), "\n")
+    s_codes := strings.Split(string(dat), "\n")
 
-    frequency := 0
-    for _, s_change := range s_changes {
-        i_change, err := strconv.Atoi(s_change)
-        if err == nil {
-            frequency += i_change
+    twos := 0
+    threes := 0
+    for _, s_code := range s_codes {
+        c := counts(s_code)
+        if c[2] != 0 {
+            twos += 1
+        }
+        if c[3] != 0 {
+            threes += 1
         }
     }
 
-    fmt.Printf("%v\n", frequency)
-    */
+    fmt.Printf("%v %v %v\n", twos, threes, twos * threes)
+
 }
