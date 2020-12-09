550389763a81dd698c6cf882c07da72e1098dc57
David Janes <david@consensas.com>
2020-12-09 05:54:01 -0500

    Day 9 done

--- 2020/day_09_2.js
+++ 2020/day_09_2.js
@@ -7,7 +7,7 @@ const parse = text => text
     .filter(line => line.length)
     .map(line => parseInt(line))
 
-const run = (codes, window) => {
+const find = (codes, window) => {
     for (let ci = window; ci < codes.length; ci++) {
         let c = codes[ci]
 
@@ -27,12 +27,32 @@ const run = (codes, window) => {
         }
 
         if (!valid) {
-            return c
+            return ci
         }
     }
 
     return null
 }
 
+const run = (codes, position) => {
+    const target = codes[position]
+
+    for (let ci = 0; ci < position; ci++) {
+        let sum = 0
+        for (let xi = ci; xi < position; xi++) {
+            sum += codes[xi]
+            if (sum === target) {
+                const range = codes.slice(ci, xi + 1)
+                console.log(range)
+                console.log(Math.min(...range) + Math.max(...range))
+                return
+            }
+        }
+    }
+
+    console.log("no answer")
+}
+
 const codes = parse(fs.readFileSync("day_09.txt", "utf-8"))
-console.log(run(codes, 25))
+const position = find(codes, 25)
+run(codes, position) 
