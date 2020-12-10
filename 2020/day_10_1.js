45c42701c154a51c9efe012f7d01966ac9934ea8
David Janes <david@consensas.com>
2020-12-10 06:26:50 -0500

    Day 10

---
+++ 2020/day_10_1.js
@@ -0,0 +1,40 @@
+// http://adventofadapter.com/2020/day/10
+"use strict"
+
+const fs = require("fs")
+const _ = require("lodash")
+
+const parse = text => text
+    .split("\n")
+    .filter(line => line.length)
+    .map(line => parseInt(line))
+
+const run = _adapters => {
+    const device = Math.max(..._adapters) + 3
+    const adapters = new Set(_adapters)
+    adapters.add(device)
+
+    const deltas = [ 0, 0, 0, 0 ]
+    const used = new Set()
+
+    const _search = v => {
+        used.add(v)
+
+        console.log(v)
+
+        for (let d = 1; d <= 3; d++) {
+            const vd = v + d
+            if (adapters.has(vd) && !used.has(vd)) {
+                deltas[d] += 1
+                _search(vd)
+            }
+        }
+    }
+
+    _search(0)
+
+    console.log(deltas)
+}
+
+const adapters = parse(fs.readFileSync("day_10b.sample", "utf-8"))
+run(adapters)
