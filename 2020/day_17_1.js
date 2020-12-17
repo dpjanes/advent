67b56a967e0f755cf047a2aa2d4dc2aca16a960c
David Janes <david@consensas.com>
2020-12-17 02:43:26 -0500

    Part 1

--- 2020/day_17_1.js
+++ 2020/day_17_1.js
@@ -11,127 +11,150 @@ const ACTIVE = "#"
 const Grid = () => {
     const self = Object.assign({})
 
-    let cx = 0;
-    let cy = 0;
-    let dx = 0;
-    let dy = -1;
-    let maxx = 0;
-    let maxy = 0;
-    let minx = 0;
-    let miny = 0;
-    let infected = 0;
+    let minx = 0
+    let maxx = 0
+    let miny = 0
+    let maxy = 0
+    let minz = 0
+    let maxz = 0
     
+    const _key = (x, y, z) => `${x}/${y}/${z}`
+
     self.data = {}
-    self.set = (x, y, c) => {
-        if (c === '.') {
-            delete self.data[`${x}/${y}`];
+    self.set = (x, y, z, c) => {
+        if (!c) {
+            delete self.data[_key(x, y, z)]
         } else {
-            self.data[`${x}/${y}`] = c;
+            self.data[_key(x, y, z)] = c
+
+            minx = Math.min(x, minx)
+            miny = Math.min(y, miny)
+            minz = Math.min(z, minz)
+
+            maxx = Math.max(x, maxx)
+            maxy = Math.max(y, maxy)
+            maxz = Math.max(z, maxz)
         }
+
     }
-    self.get = (x, y) => self.data[`${x}/${y}`] || ".";
+    self.get = (x, y, z) => !!self.data[_key(x, y, z)]
 
     self.initialize = raw => {
         raw.split("\n")
             .filter(line => line.length)
             .forEach((line, y, lines) => {
-                y -= (lines.length - 1) / 2
+                // y -= (lines.length - 1) / 2
                 maxy = Math.max(y, maxy)
                 miny = Math.min(y, miny)
 
                 line.split("")
                     .forEach((c, x, cs) => {
-                        x -= (cs.length - 1) / 2
+                        // x -= (cs.length - 1) / 2
                         maxx = Math.max(x, maxx)
                         minx = Math.min(x, minx)
-
-                        self.set(x, y, c)
+                        
+                        self.set(x, y, 0, c === '#')
                     })
             })
     }
 
-    /*
-    self.reverse = () => {
-        dx = -dx;
-        dy = -dy;
-    }
-
-    self.right = () => {
-        if (dx === 1) {
-            dx = 0;
-            dy = 1;
-        } else if (dx === -1) {
-            dx = 0;
-            dy = -1;
-        } else if (dy === 1) {
-            dy = 0;
-            dx = -1;
-        } else if (dy === -1) {
-            dy = 0;
-            dx = 1;
-        } else {
-            assert.ok(false);
-        }
-    }
-
-    self.left = () => {
-        if (dx === 1) {
-            dx = 0;
-            dy = -1;
-        } else if (dx === -1) {
-            dx = 0;
-            dy = 1;
-        } else if (dy === 1) {
-            dy = 0;
-            dx = 1;
-        } else if (dy === -1) {
-            dy = 0;
-            dx = -1;
-        } else {
-            assert.ok(false);
-        }
-    }
-    */
-
     self.pretty = () => {
-        const cs = [];
-
-        for (let y = miny; y <= maxy; y++) {
-            if (y !== miny) {
-                cs.push("\n")
+        console.log("----")
+        for (let z = minz; z <= maxz; z++) {
+            console.log(`z=${z}`)
+            for (let y = miny; y <= maxy; y++) {
+                const cs = []
+                for (let x = minx; x <= maxx; x++) {
+                    cs.push(self.get(x, y, z) ? "#" : ".")
+                }
+                console.log(cs.join(""))
             }
-            cs.push("")
+        }
+    }
 
-            for (let x = minx; x <= maxx; x++) {
-                if (false && (x === cx) && (y === cy)) {
-                    cs.push("[" + self.get(x, y) + "]")
-                } else {
-                    cs.push("" + self.get(x, y) + "")
+    self.iterate = () => {
+        const ngrid = Grid()
+
+        for (let z = minz - 1; z <= maxz + 1; z++) {
+// console.log()
+            for (let y = miny - 1; y <= maxy + 1; y++) {
+// let counts = []
+                for (let x = minx - 1; x <= maxx + 1; x++) {
+                    let count = 0
+
+                    for (let dz = -1; dz <= 1; dz++) {
+                        for (let dy = -1; dy <= 1; dy++) {
+                            for (let dx = -1; dx <= 1; dx++) {
+                                if ((dx === 0) && (dy === 0) && (dz === 0)) {
+                                    continue
+                                } else if (self.get(x + dx, y + dy, z + dz)) {
+                                    count += 1
+                                }
+                            }
+                        }
+                    }
+
+// counts.push(count)
+                    if (self.get(x, y, z)) {
+                        if ((count === 2) || (count === 3)) {
+                            ngrid.set(x, y, z, true)
+                        } else {
+                            ngrid.set(x, y, z, false)
+                        }
+                    } else {
+                        if (count === 3) {
+                            ngrid.set(x, y, z, true)
+                        } else {
+                            ngrid.set(x, y, z, false)
+                        }
+                    }
+
+                    // console.log(x, y, z, "=", count)
+                }
+// console.log(counts.join(""))
+            }
+        }
+
+        return ngrid
+    }
+
+    self.count = () => {
+        let count = 0
+
+        for (let z = minz; z <= maxz; z++) {
+            for (let y = miny; y <= maxy; y++) {
+                for (let x = minx; x <= maxx; x++) {
+                    count += self.get(x, y, z) ? 1 : 0
                 }
             }
         }
 
-        return cs.join("")
+        return count
     }
 
-    return self;
+    return self
 }
 
 const run = (raw, iterations) => {
     let grid = Grid()
     grid.initialize(raw)
-    console.log(grid.pretty())
+    grid.pretty()
 
     for (let i = 0; i < iterations; i++) {
+        /*
         if ((i % 10000) === 0) {
             console.log("*", i)
         }
+        */
 
-        // grid.move()
+        grid = grid.iterate()
+        // grid.pretty()
+
     }
 
-    // return grid.infected();
+    console.log("COUNT", grid.count())
+
 
 }
 
-run(fs.readFileSync("day_17.sample", "utf-8"), 1)
+run(fs.readFileSync("day_17.txt", "utf-8"), 6)
