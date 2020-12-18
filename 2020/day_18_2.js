425e48d1ff6f91ad6fa6bcdf3ec3aa1a1fd7d0eb
David Janes <david@consensas.com>
2020-12-18 05:06:39 -0500

    Day 18 solved

--- 2020/day_18_2.js
+++ 2020/day_18_2.js
@@ -6,7 +6,8 @@ const _ = require("lodash")
 const assert = require("assert")
 
 const bracket_rex = new RegExp(/[(]([^()]*)[)]/)
-const math_rex = new RegExp(/(\d+)\s*([+*])\s*(\d+)/)
+const add_rex = new RegExp(/(\d+)\s*([+])\s*(\d+)/)
+const mul_rex = new RegExp(/(\d+)\s*([*])\s*(\d+)/)
 const evaluate = input => {
     let str = input
     
@@ -26,14 +27,26 @@ const evaluate = input => {
 
     assert.strictEqual(str.indexOf("("), -1)
 
-    // do math
-    let result = 0
-
+    // do add
     while (true) {
-        const nstr = str.replace(math_rex, (match, a, op, b) => {
+        const nstr = str.replace(add_rex, (match, a, op, b) => {
             if (op === "+") {
                 return parseInt(a) + parseInt(b)
-            } else if (op === "*") {
+            } else {
+                assert.ok(false)
+            }
+        })
+
+        if (nstr === str) {
+            break
+        }
+        str = nstr
+    }
+
+    // do mul
+    while (true) {
+        const nstr = str.replace(mul_rex, (match, a, op, b) => {
+            if (op === "*") {
                 return parseInt(a) * parseInt(b)
             } else {
                 assert.ok(false)
@@ -49,10 +62,10 @@ const evaluate = input => {
     return parseInt(str)
 }
 
-assert.strictEqual(evaluate("2 * 3 + (4 * 5)"), 26)
-assert.strictEqual(evaluate("5 + (8 * 3 + 9 + 3 * 4 * 3)"), 437)
-assert.strictEqual(evaluate("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))"), 12240)
-assert.strictEqual(evaluate("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2"), 13632)
+assert.strictEqual(evaluate("2 * 3 + (4 * 5)"), 46)
+assert.strictEqual(evaluate("5 + (8 * 3 + 9 + 3 * 4 * 3)"), 1445)
+assert.strictEqual(evaluate("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))"), 669060)
+assert.strictEqual(evaluate("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2"), 23340)
 
 const lines = fs.readFileSync("day_18.txt", "utf-8").split("\n").filter(line => line.length)
 
