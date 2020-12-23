92a0e7617b3bade2b0dc6e1e8ed809dd4f93b994
David Janes <david@consensas.com>
2020-12-23 09:14:48 -0500

    rewritten efficiently

---
+++ 2020/day_23_2.js
@@ -0,0 +1,140 @@
+// http://adventofcode.com/2020/day/23
+const fs = require("fs")
+const assert = require("assert")
+
+const Circle = () => {
+    const self = new Object({})
+
+    let currentv = 0
+    let maxv = 0
+    let state = []
+    let valued = {}
+
+    self.push = value => {
+        maxv = Math.max(value, maxv)
+
+        if (state.length === 0) {
+            state.push({
+                value: value,
+                next: 0,
+                // prev: 0,
+            })
+        } else {
+            state[state.length - 1].next = state.length
+            state.push({
+                value: value,
+                next: 0,
+                // prev: state.length,
+            })
+        }
+
+        const last = state[state.length - 1]
+        valued[last.value] = last
+    }
+
+    self.initialize = s => {
+        s.replace(/[^\d]/g, "")
+            .split("")
+            .map(s => parseInt(s))
+            .forEach(i => self.push(i))
+
+        currentv = state[0].value
+    }
+
+    self.flat = () => {
+        const parts = []
+
+        let cursor = valued[1]
+        for (let i = 1; i < state.length; i++) {
+            cursor = state[cursor.next]
+            parts.push(`${cursor.value}`)
+        }
+
+        return parts.join("")
+    }
+
+    self.prettys = () => {
+        const parts = []
+
+        let cursor = valued[currentv]
+        for (let i = 0; i < state.length; i++) {
+            if (i === 0) {
+                parts.push(`(${cursor.value})`)
+            } else {
+                parts.push(`${cursor.value}`)
+            }
+
+            cursor = state[cursor.next]
+        }
+
+        return parts.join(" ")
+    }
+
+    self.state = () => {
+        state.forEach((d, x) => {
+            console.log(x, d)
+        })
+    }
+
+    self.play = MOVES => {
+        let nmove = 0
+        const move = () => {
+            console.log(`-- move ${++nmove} --`)
+            console.log(`cups: ${self.prettys()}`)
+
+            const currentd = valued[currentv]
+            console.log("CURRENT", currentd)
+            let popd = currentd
+            const popds = []
+            for (let i = 0; i < 3; i++) {
+                popd = state[popd.next]
+                popd.X = true
+                popds.push(popd)
+                console.log("POP", popd)
+            }
+
+            let nextv = currentv
+            while (true) {
+                --nextv
+
+                if (nextv === 0) {
+                    nextv = maxv
+                }
+
+                if ((nextv !== popds[0].value) && (nextv !== popds[1].value) && (nextv !== popds[2].value)) {
+                    break
+                }
+            }
+
+            const nextd = valued[nextv]
+
+            const nextd_next = nextd.next
+            const currentd_next = currentd.next
+
+            currentd.next = popds[2].next
+            nextd.next = currentd_next
+            popds[2].next = nextd_next
+
+            /*
+            console.log("NEXT", nextd)
+            self.state()
+            */
+
+            currentv = state[currentd.next].value
+        }
+
+
+        for (let mi = 0; mi < MOVES; mi++) {
+            move()
+        }
+
+        console.log("FINAL", self.flat())
+    }
+
+    return self
+}
+
+const circle = Circle()
+circle.initialize("389125467") // test
+// circle.initialize("562893147") // real
+circle.play(10)
