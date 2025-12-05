4a5e7dcdf4860316e6fe22fe5605d675d8b0c9a6
David Janes <davidjanes@gmail.com>
2025-12-05 06:02:04 -0500

    Day 5 complete

--- 2025/day_05/part_1.py
+++ 2025/day_05/part_1.py
@@ -1,11 +1,26 @@
 import re 
 import sys
 
+DB: list[tuple[int, int]] = []
+
 def main(filename: str):
     sum = 0
     with open(filename, 'r') as fin:
         for line in fin:
             line = line.strip()
+            if not line:
+                continue
+
+            parts = line.split("-")
+            if len(parts) == 2:
+                DB.append((int(parts[0]), int(parts[1])))
+                
+            if len(parts) == 1:
+                target = int(parts[0])
+                for a, b in DB:
+                    if a <= target <= b:
+                        sum += 1
+                        break
 
 
     print(sum)
--- 2025/day_05/part_2.py
+++ 2025/day_05/part_2.py
@@ -1,12 +1,36 @@
 import re 
 import sys
 
+DB: list[list[int, int]] = []
+
 def main(filename: str):
-    sum = 0
     with open(filename, 'r') as fin:
         for line in fin:
             line = line.strip()
+            if not line:
+                continue
 
+            parts = line.split("-")
+            if len(parts) == 2:
+                DB.append([ int(parts[0]), int(parts[1])])
+
+    DB.sort()
+
+    merged: list[list[int, int]] = []
+    current = DB[0]
+    merged.append(current)
+
+    for a, b in DB[1:]:
+        if a <= current[1]:
+            current[1] = max(current[1], b)
+        else:
+            current = [a, b]
+            merged.append(current)
+
+    sum = 0
+    for a, b in merged:
+        print(a, b, b - a + 1)
+        sum += b - a + 1
 
     print(sum)
 
