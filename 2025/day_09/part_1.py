a6297eb1fdc3ae86b8a5d830c5cc12a86a03dc34
David Janes <davidjanes@gmail.com>
2025-12-09 04:22:48 -0500

    Day 9/1

--- 2025/day_09/part_1.py
+++ 2025/day_09/part_1.py
@@ -1,14 +1,51 @@
 import re 
 import sys
 
+DB: set[tuple[int, int]] = set()
+ROW_DB: dict[int, list[tuple[int, int]]] = {}
+COL_DB: dict[int, list[tuple[int, int]]] = {}
+
 def main(filename: str):
-    sum = 0
     with open(filename, 'r') as fin:
         for line in fin:
             line = line.strip()
+            parts = line.split(',')
+            if len(parts) != 2:
+                continue
 
+            col, row = map(int, parts)
+            DB.add((col, row))
 
-    print(sum)
+            ROW_DB.setdefault(row, []).append((col, row))
+            COL_DB.setdefault(col, []).append((col, row))
+
+    for val in ROW_DB.values():
+        val.sort()
+    for val in COL_DB.values():
+        val.sort(key=lambda x: x[1])
+
+    biggest = 0
+    biggest_a = None
+    biggest_b = None
+    for a in DB:
+        for b in DB:
+            if a == b:
+                continue
+
+            col_diff = abs(a[0] - b[0]) + 1
+            row_diff = abs(a[1] - b[1]) + 1
+            size = col_diff * row_diff
+            # print(a, b, size)
+            if size < biggest:
+                continue
+
+            biggest = size
+            biggest_a = a
+            biggest_b = b
+
+    # print(ROW_DB)
+    print(biggest)
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
