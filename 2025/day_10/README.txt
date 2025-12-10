f4c1f48ed7272d19740807708170848904c432da
David Janes <davidjanes@gmail.com>
2025-12-10 05:57:57 -0500

    parse input data

--- 2025/day_10/README.txt
+++ 2025/day_10/README.txt
@@ -9,6 +9,7 @@ For example:
 [.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
 [...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}
 [.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}
+
 The manual describes one machine per line. Each line contains a single indicator light diagram in [square brackets], one or more button wiring schematics in (parentheses), and joltage requirements in {curly braces}.
 
 To start a machine, its indicator lights must match those shown in the diagram, where . means off and # means on. The machine has the number of indicator lights shown, but its indicator lights are all initially off.
--- 2025/day_10/part_1.py
+++ 2025/day_10/part_1.py
@@ -1,14 +1,50 @@
 import re 
 import sys
+import dataclasses
+
+'''
+[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
+[...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}
+[.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}'''
+
+@dataclasses.dataclass
+class Row:
+    lights: set[int] = dataclasses.field(default_factory=set)
+    wirings: list[list[int]] = dataclasses.field(default_factory=list)
+    joltages: list[int] = dataclasses.field(default_factory=list)
+
+    @classmethod
+    def from_line(cls, line: str) -> 'Row':
+        line = line.strip()
+        if not line:
+            return None
+        
+        row = cls()
+        
+        parts = line.split()
+        for part in parts:
+            if part[0] == '[':
+                part = part[1:-1]
+                row.lights = {chx for chx, ch in enumerate(part) if ch == '#'}
+            elif part[0] == '(':
+                part = part[1:-1]
+                row.wirings.append([int(x) for x in part.split(',')])
+            elif part[0] == '{':
+                part = part[1:-1]
+                row.joltages = [int(x) for x in part.split(',')]
+
+        return row
 
 def main(filename: str):
-    sum = 0
     with open(filename, 'r') as fin:
         for line in fin:
-            line = line.strip()
+            row = Row.from_line(line)
+            if row is None:
+                continue
 
-
-    print(sum)
+            # print(line.strip())
+            # print(row)
+            # print()
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
