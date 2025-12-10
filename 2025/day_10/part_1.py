8ef3243324f98c603c0bb2433e90509534586d7f
David Janes <davidjanes@gmail.com>
2025-12-10 07:26:17 -0500

    10/1

--- 2025/day_10/part_1.py
+++ 2025/day_10/part_1.py
@@ -34,13 +34,76 @@ class Row:
                 row.joltages = [int(x) for x in part.split(',')]
 
         return row
+    
+def solve_depth_first(row: Row, state: set[int], wiring: list[int], steps: list[list[int]]) -> list[list[int]]:
+    # print("X", wiring)
+    state = set(state)
+    for wx in wiring or []:
+        if wx in state:
+            state.remove(wx)
+        else:
+            state.add(wx)
+
+    if state == row.lights:
+        # print("Found solution:", wiring)
+        return steps
+    
+    if len(steps) > 5:
+        # print("Max depth reached")
+        return
+
+    for wiring in row.wirings:
+        solution = solve_depth_first(row, state, wiring, steps + [ wiring ])
+        if solution:
+            return solution
+        
+def solve_breadth_first(row: Row) -> list[list[int]]:
+    stack = [
+        ( set(), [], [] )
+    ]
+    stack_index = 0
+    while stack_index < len(stack):
+        state, wiring, steps = stack[stack_index]
+        stack_index += 1
+
+        state = set(state)
+        for wx in wiring:
+            if wx in state:
+                state.remove(wx)
+            else:
+                state.add(wx)
+
+        if state == row.lights:
+            return steps
+        
+        if len(steps) > 10:
+            print("Max depth reached")
+            continue
+
+        for wiring in row.wirings:
+            stack.append( ( state, wiring, steps + [ wiring ] ) )
+    pass
+
 
 def main(filename: str):
+    rows: list[Row] = []
     with open(filename, 'r') as fin:
         for line in fin:
             row = Row.from_line(line)
             if row is None:
                 continue
+
+            rows.append(row)
+
+    count = 0
+    for row in rows:
+        solution = solve_breadth_first(row)
+        count += len(solution) if solution else 0
+        print("Solution:", solution)
+
+    print("Total count:", count)
+
+
 
             # print(line.strip())
             # print(row)
