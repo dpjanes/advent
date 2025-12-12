cc4a4c23043b38e25ab663915288b3510d445d52
David Janes <davidjanes@gmail.com>
2025-12-12 06:48:32 -0500

    Day 12 roughin

--- 2025/day_11/part_2.py
+++ 2025/day_11/part_2.py
@@ -1,14 +1,58 @@
 import re 
 import sys
 
+DB: dict[str, str] = {}
+
 def main(filename: str):
-    sum = 0
     with open(filename, 'r') as fin:
         for line in fin:
             line = line.strip()
+            if not line:
+                continue
+
+            d_from, back = line.split(":", 1)
+            d_tos = back.split()
+
+            for d_to in d_tos:
+                DB.setdefault(d_from, []).append(d_to)
+
+    print(DB)
 
 
-    print(sum)
+    solved: dict[str, int] = {}
+
+    def solve(d_to: str, seen: set):
+        # print("VISIT", d_to, DB.get(d_to, []))
+        if d_to in seen:
+            return 0
+        
+        if d_to in solved:
+            print("SKIP", d_to, solved[d_to])
+            return solved[d_to]
+
+        if d_to == "out":
+            # print("HERE", seen, "fft" in seen, "dac" in seen)
+            if "fft" in seen and "dac" in seen:
+                # print("FOUND ONE")
+                return 1
+            else:
+                return 0
+        
+        seen = seen | {d_to}
+        
+        result = 0
+        for d_from in DB.get(d_to, []):
+            subresult = solve(d_from, seen)
+            solved[d_from] = subresult
+            result += subresult
+
+        # print("SOLUTION", d_to, result)
+        return result
+
+
+
+    result = solve("svr", set())
+    print(result)
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
